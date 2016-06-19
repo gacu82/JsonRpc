58873e0ad9685ec48c77989a8ef01a59ce376550
Krzysztof Szczuka <krzysztof.szczuka@fleetadmin.pl>
2016-06-19 16:15:34 +0200

    build script changes

--- build-host.bat
+++ build-host.bat
@@ -1,1 +1,3 @@
-dotnet build src\JsonRpc.Host\project.json --configuration Release && dotnet test test\JsonRpc.Tests\project.json
+dotnet restore &&^
+dotnet build src\JsonRpc.Host\project.json --configuration Release &&^
+dotnet test test\JsonRpc.Tests\project.json
